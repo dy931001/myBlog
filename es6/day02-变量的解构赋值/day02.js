26083edcac2badaadbf2fb2e08f9fa85da4dba8f
lideyao <1125168527@qq.com>
2018-09-18 17:56:20 +0800

    es6

---
+++ es6/day02-变量的解构赋值/day02.js
@@ -0,0 +1,22 @@
+//数组的解构赋值
+let [a, b, c] = [1, 2, 3];
+let [foo, [[bar], baz]] = [1, [[2], 3]];
+foo // 1
+bar // 2
+baz // 3
+
+let [ , , third] = ["foo", "bar", "baz"];
+third // "baz"
+
+let [x, , y] = [1, 2, 3];
+x // 1
+y // 3
+
+let [head, ...tail] = [1, 2, 3, 4];
+head // 1
+tail // [2, 3, 4]
+
+let [x, y, ...z] = ['a'];
+x // "a"
+y // undefined
+z // []
